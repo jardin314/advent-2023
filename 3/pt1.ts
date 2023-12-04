6d94a2f3e8fb748aee5b2620ace9c0a3c0a79ab3
Jordan Coe <jcoe@mynetsvs.com>
2023-12-03 23:01:07 -0500

    messy but functioning solution for day 3 pt 1

--- 3/pt1.ts
+++ 3/pt1.ts
@@ -2,7 +2,6 @@ import * as fs from 'node:fs/promises'
 import 'dotenv/config'
 
 async function main() {
-  console.log(process.env.PATH_TO_ROOT)
   const input: string = await fs.readFile(`${process.env.PATH_TO_ROOT}/3/input`, 'utf8')
   const inputArray: Array<string> = input.trim().split('\n')
   let total: number = 0
@@ -11,8 +10,7 @@ async function main() {
     const line = inputArray[i] ?? ''
     const previousLine = inputArray[i-1] ?? ''
     const nextLine = inputArray[i+1] ?? ''
-    const nums: string[]|null = line.match(/\d+/)
-    // const numIndexMap = new Map()
+    const nums: string[]|null = line.match(/\d+/g)
 
     if (nums === null) {
       continue
@@ -21,24 +19,33 @@ async function main() {
     let pointInLine: number = 0
 
     for (let num of nums) {
-      const numIndexInLine: number = pointInLine + line.slice(pointInLine).indexOf(num)
-      // numIndexMap.set(numIndexInLine, num)
-      pointInLine += numIndexInLine + num.length
+      pointInLine += line.slice(pointInLine).indexOf(num)
+
       let addToTotal: boolean = false 
-      for (let i=0; i < num.length; i++) {
+
+      for (let j=0; j < num.length; j++) {
         const nonSymbols: string[] = ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-       if (nonSymbols.includes(line[numIndexInLine+i-1]) ||
-           nonSymbols.includes(line[numIndexInLine+i+1]) ||
-           nonSymbols.includes(previousLine[numIndexInLine+i-1]) ||
-           nonSymbols.includes(previousLine[numIndexInLine+i]) ||
-           nonSymbols.includes(previousLine[numIndexInLine+i+1]) ||
-           nonSymbols.includes(nextLine[numIndexInLine+i-1]) ||
-           nonSymbols.includes(nextLine[numIndexInLine+i]) ||
-           nonSymbols.includes(nextLine[numIndexInLine+i+1])
+
+       if (
+           (pointInLine !== 0 && !nonSymbols.includes(line[pointInLine-1])) ||
+           (pointInLine !== line.length-1 && !nonSymbols.includes(line[pointInLine+1])) ||
+           (
+            !!previousLine &&
+            ((pointInLine !== 0 && !nonSymbols.includes(previousLine[pointInLine-1])) ||
+            !nonSymbols.includes(previousLine[pointInLine]) ||
+            (pointInLine !== line.length-1 && !nonSymbols.includes(previousLine[pointInLine+1])))
+           )
+             ||
+           (
+            !!nextLine &&
+            ((pointInLine !== 0 && !nonSymbols.includes(nextLine[pointInLine-1])) ||
+            !nonSymbols.includes(nextLine[pointInLine]) ||
+            (pointInLine !== line.length-1 && !nonSymbols.includes(nextLine[pointInLine+1])))
+           )
           ) {
             addToTotal = true
-            break
           }
+        pointInLine++
       }
       if (addToTotal) {
         total+= Number(num)
