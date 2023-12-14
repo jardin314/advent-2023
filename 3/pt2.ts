486f0d508b146b780e02188832838beef9484219
Jordan Coe <jcoe@mynetsvs.com>
2023-12-14 14:25:48 -0500

    Fix several issues with coordinate assignments and number checking to finally get working solution for day 3 pt 2

--- 3/pt2.ts
+++ 3/pt2.ts
@@ -8,6 +8,7 @@ type Coordinates = {
 type NumberGridInfo = {
     coordinateList: Array<Coordinates>,
     value: string,
+    id: number,
 }
 
 
@@ -18,6 +19,7 @@ async function main() {
   const numberIndicesArray: Array<NumberGridInfo> = []
   let total: number = 0
   const gearIndicesArray: Array<Coordinates> = []
+  let numId: number = 0
 
   for (let i=0; i < inputArray.length; i++) {
     const line = inputArray[i] ?? ''
@@ -33,7 +35,7 @@ async function main() {
       for (let n=0; n < nums.length; n++) {
         const num = nums[n]
         pointInLine += line.slice(pointInLine).indexOf(num)
-        const numberInfo: NumberGridInfo = {coordinateList: [], value: num}
+        const numberInfo: NumberGridInfo = {coordinateList: [], value: num, id: numId++}
 
         for (let j=pointInLine; j < pointInLine + num.length; j++) {
           const coordinates: Coordinates = {x: j, y: i}
@@ -48,8 +50,8 @@ async function main() {
       let pointInLine: number = 0
 
       for (const gear of gears) {
-        pointInLine += line.slice(pointInLine).indexOf(gear)
-        const coordinatesOfGear: Coordinates = {x: pointInLine, y: i}
+        pointInLine += line.slice(pointInLine).indexOf(gear) + 1
+        const coordinatesOfGear: Coordinates = {x: pointInLine-1, y: i}
         gearIndicesArray.push(coordinatesOfGear)
       }
     }
@@ -61,7 +63,7 @@ async function main() {
   return total
 
   function calculateGear(gearIndex: Coordinates, numberIndicesArray: Array<NumberGridInfo>): number {
-    const valuesToMultiply: number[] = []
+    const valuesToMultiply: any[] = []
     let indicesToCheck: Array<Coordinates> = [
       {x: gearIndex.x-1, y: gearIndex.y-1},
       {x: gearIndex.x, y: gearIndex.y-1},
@@ -79,15 +81,20 @@ async function main() {
             return value.coordinateList.some((coord) => coord.x === index.x && coord.y === index.y)
           })
         if (!!number) {
-          for (const coordinate of number?.coordinateList) {
-            indicesToCheck = indicesToCheck.filter((value) => value.x !== coordinate.x || value.y !== coordinate.y)
+          let addToValuesToMultiply = true
+          for (let value of valuesToMultiply) {
+            if (value.id === number.id) {
+              addToValuesToMultiply = false
+            }
           }
-          valuesToMultiply.push(Number(number.value))
+          if (addToValuesToMultiply) {
+            valuesToMultiply.push(number)
+          }
         }
       }
     }
     if (valuesToMultiply.length === 2) {
-      return valuesToMultiply[0] * valuesToMultiply[1]
+      return Number(valuesToMultiply[0].value) * Number(valuesToMultiply[1].value)
     } else {
       return 0
     }
