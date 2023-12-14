ab137df2a3bad403130cbd2053ab72b29bfe7afc
Jordan Coe <jcoe@mynetsvs.com>
2023-12-14 15:08:55 -0500

    overwrite day 4 pt1 with not-yet-functioning solution for pt2

--- 4/pt1.ts
+++ 4/pt1.ts
@@ -4,22 +4,27 @@ import 'dotenv/config'
 async function main() {
   let total: number = 0
   const input: string = await fs.readFile(`${process.env.PATH_TO_ROOT}/4/input`, 'utf8')
-  const inputArray: Array<string> = input.trim().split('\n')
-  for (let line of inputArray) {
-    let score: number = 0
-    line = line.slice(line.indexOf(':') + 1)
+  const inputArray: string[] = input.trim().split('\n')
+
+  for (let i=0; i < inputArray.length; i++) {
+    let matches: number = 0
+    const line: string = inputArray[i].slice(inputArray[i].indexOf(':') + 1)
     const winningNumbers: string[]|null = line.split('|')[0].match(/\d+/g)
     const myNumbers: string[]|null = line.split('|')[1].match(/\d+/g)
     
     for (const number of myNumbers?? '') {
       if (winningNumbers?.indexOf(number) !== -1) {
-        score = score === 0 ? 1 : score*2
+        matches++
       }
     }
-    total += score
+    for (let j=0; j < matches; j++) {
+      let locationToAdd = i + j + 1
+      inputArray.splice(locationToAdd, 0, inputArray[locationToAdd+1])
+    }
+    break
   }
 
-  return total
+  return inputArray.length
 }
 
 
