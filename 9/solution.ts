734de31b27b0f3f50d9a581d5184ecc12c838266
Jordan Coe <jardinc97@gmail.com>
2023-12-19 09:53:12 -0500

    solution for day 9 pt 1

--- 9/solution.ts
+++ 9/solution.ts
@@ -1,12 +1,46 @@
 import * as fs from 'node:fs/promises'
 import 'dotenv/config'
 
-type leftRightDirections = {left: string, right: string}
-type MapDirections = Map<string|undefined, leftRightDirections>
 
 async function main() {
-  const input: string = await fs.readFile(`${process.env.PATH_TO_ROOT}/8/input`, 'utf8')
+  const input: string = await fs.readFile(`${process.env.PATH_TO_ROOT}/9/input`, 'utf8')
   const inputArray: string[] = input.trim().split('\n')
+  let sumOfAllNextValues: number = 0
+  for (let line of inputArray) {
+    const sequence: number[] = line.split(' ').map((value) => Number(value))
+    const nextInSequence = getNextValue(line, sequence)   
+    sumOfAllNextValues += nextInSequence
+  }
+  return sumOfAllNextValues
+}
+
+function getNextValue(line: string, sequence: number[]) {
+  const sequences: Array<number[]> = [sequence]
+  if (sequences.at(-1) === undefined) {
+    throw Error('Nothing in sequences')
+  }
+  let currentIndex: number = 0
+  while (sequences.at(-1)?.some((value) => value !== 0)) {
+    const currentSequence: number[] = sequences[currentIndex]
+    const nextSequence: number[] = []
+    for (let i = 1; i < currentSequence.length; i++) {
+      nextSequence.push(currentSequence[i] - currentSequence[i-1])
+    }
+    sequences.push(nextSequence)
+    currentIndex++
+  }
+  for (let i = sequences.length-2; i >=0; i--) {
+    const valueToAdd = sequences[i+1].at(-1) ?? NaN
+    const currentLastInSequence = sequences[i].at(-1) ?? NaN
+    const nextInSequence = currentLastInSequence + valueToAdd
+    if (isNaN(nextInSequence)) {
+      throw Error('Number in sequence is not a number')
+    }
+    sequences[i].push(nextInSequence)
+  }
+  const nextInFirstSequence = sequences[0].at(-1) ?? NaN
+
+  return nextInFirstSequence
 }
 
 
